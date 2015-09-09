63eb1899fc337775b0cf8bd75783e352ef99ac5c
Adam Krebs <amk528@cs.nyu.edu>
2015-09-09 16:52:13 -0400

    update d3 logic. closes #6

--- backbone.d3view.js
+++ backbone.d3view.js
@@ -22,8 +22,21 @@
   // Events need a unique id for attaching multiple events of the same type.
   var uniqueId = 0;
 
-  // Store eventsMap for undelegation.
-  var _eventsMap = {};
+  var ElementProto = (typeof Element !== 'undefined' && Element.prototype) || {};
+  var matchesSelector = ElementProto.matches ||
+    ElementProto.webkitMatchesSelector ||
+    ElementProto.mozMatchesSelector ||
+    ElementProto.msMatchesSelector ||
+    ElementProto.oMatchesSelector ||
+    // Make our own `Element#matches` for IE8
+    function(selector) {
+      // Use querySelectorAll to find all elements matching the selector,
+      // then check if the given element is included in that list.
+      // Executing the query on the parentNode reduces the resulting nodeList,
+      // (document doesn't have a parentNode).
+      var nodeList = (this.parentNode || document).querySelectorAll(selector) || [];
+      return ~indexOf(nodeList, this);
+    };
 
   Backbone.D3ViewMixin = {
 
@@ -42,12 +55,11 @@
 
     _removeElement: function() {
       this.undelegateEvents();
-      delete _eventsMap[this.cid];
       this.d3el.remove();
     },
 
     _createElement: function(tagName) {
-      var ns = _.result(this, 'namespace');
+      var ns = typeof this.namespace === 'function' ? this.namespace() : this.namespace;
       return ns ?
          document.createElementNS(ns, tagName) :
          document.createElement(tagName);
@@ -82,82 +94,73 @@
         selector = null;
       }
 
-      var map = _eventsMap[this.cid] || (_eventsMap[this.cid] = {}),
+      var map = this._eventHandlers || (this._eventHandlers = {}),
           handlers = map[eventName] || (map[eventName] = []);
 
-      handlers.push({selector: selector, listener: listener});
-
-      var ElementProto = (typeof Element !== 'undefined' && Element.prototype) || {};
-      var matchesSelector = ElementProto.matches ||
-        ElementProto.webkitMatchesSelector ||
-        ElementProto.mozMatchesSelector ||
-        ElementProto.msMatchesSelector ||
-        ElementProto.oMatchesSelector ||
-        // Make our own `Element#matches` for IE8
-        function(selector) {
-          // Use querySelectorAll to find all elements matching the selector,
-          // then check if the given element is included in that list.
-          // Executing the query on the parentNode reduces the resulting nodeList,
-          // (document doesn't have a parentNode).
-          var nodeList = (this.parentNode || document).querySelectorAll(selector) || [];
-          return ~indexOf(nodeList, this);
-        };
-
-      el.addEventListener(eventName, function(event){
+      var view = this;
+      var wrapped = function(event){
         var node = event.target,
             idx = 0,
             o = d3.event;
 
         d3.event = event;
+
         // The `event` object is stored in `d3.event` but Backbone expects it as
         // the first argument to the listener.
-        if(! selector){
-          listener.apply(this, [d3.event, node.__data__, idx++]);
+        if (!selector) {
+          listener.call(view, d3.event, node.__data__, idx++);
+          d3.event = o;
           return;
         }
-        while(node && node !== el){
-          if(matchesSelector.call(node, selector)){
-            listener.apply(this, [d3.event, node.__data__, idx++]);
+
+        while (node && node !== el){
+          if (matchesSelector.call(node, selector)) {
+            listener.call(view, d3.event, node.__data__, idx++);
           }
           node = node.parentNode;
         }
         d3.event = o;
-      }.bind(this));
+      };
+
+      handlers.push({selector: selector, listener: listener, wrapped: wrapped});
+
+      el.addEventListener(eventName, wrapped, false);
       return this;
     },
 
     undelegate: function(eventName, selector, listener) {
+      if (!this._eventHandlers) return;
+
       if (typeof selector !== 'string') {
         listener = selector;
         selector = null;
       }
 
-      var map = _eventsMap[this.cid] || (_eventsMap[this.cid] = {}),
-          handlers = map[eventName];
+      var handlers = this._eventHandlers[eventName];
+      var el = this.el;
 
-      _(handlers).chain()
+      handlers
         .filter(function(handler) {
           return (listener ? handler.listener === listener : true) &&
             (selector ? handler.selector === selector : true);
         })
         .forEach(function(handler) {
-          removeEvent(this.d3el, eventName, selector || handler.selector);
-          handlers.splice(_.indexOf(handlers, handler), 1);
-        }, this);
+          el.removeEventListener(eventName, handler.wrapped, false);
+          handlers.splice(handlers.indexOf(handler), 1);
+        });
     },
 
     undelegateEvents: function() {
-      if (!this.d3el) return;
+      var map = this._eventHandlers, el = this.el;
+      if (!el || !map) return;
 
-      var map = _eventsMap[this.cid] || (_eventsMap[this.cid] = {});
+      Object.keys(map).forEach(function(eventName) {
+        map[eventName].forEach(function(handler) {
+          el.removeEventListener(eventName, handler.wrapped, false);
+        });
+      });
 
-      for (var eventName in map) {
-        _.each(map[eventName], function(handler) {
-          removeEvent(this.d3el, eventName, handler.selector);
-        }, this);
-      }
-
-      _eventsMap[this.cid] = {};
+      this._eventHandlers = {};
       return this;
     }
   };
